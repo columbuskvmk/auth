e5a94b80d6b827578eab2ae8716eef97b91e20d8
manoj <manoj@192.168.0.4>
2018-07-14 19:51:52 +0530

    dao and impl

---
+++ Auth/src/com/auth/dao/LoginDao.java
@@ -0,0 +1,8 @@
+package com.auth.dao;
+
+public interface LoginDao {
+	
+	public String getDetails(String name);
+	
+
+}
---
+++ Auth/src/com/auth/dao/LoginDaoImpl.java
@@ -0,0 +1,11 @@
+package com.auth.dao;
+
+public class LoginDaoImpl implements LoginDao {
+
+	@Override
+	public String getDetails(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
